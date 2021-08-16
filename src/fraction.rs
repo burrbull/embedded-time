bb7310aebb2c988c349b8dc2d7d0bb2db1623308
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-08-16 11:46:41 +0300

    rm Ratio

--- src/fraction.rs
+++ src/fraction.rs
@@ -2,7 +2,6 @@
 use crate::ConversionError;
 use core::cmp::Ordering;
 use core::ops;
-use num::{rational::Ratio, CheckedDiv, CheckedMul, Zero};
 
 /// A fractional value
 ///
@@ -13,8 +12,13 @@ use num::{rational::Ratio, CheckedDiv, CheckedMul, Zero};
 /// [`Rate`]: rate/trait.Rate.html
 /// [`Clock`]: clock/trait.Clock.html
 /// [`Instant`]: instant/struct.Instant.html
-#[derive(Copy, Clone, Hash, Debug)]
-pub struct Fraction(Ratio<u32>);
+#[derive(Copy, Clone, Debug)]
+pub struct Fraction {
+    /// Numerator.
+    numer: u32,
+    /// Denominator.
+    denom: u32,
+}
 
 impl Fraction {
     /// Construct a new `Fraction`.
@@ -22,33 +26,77 @@ impl Fraction {
     /// A reduction is **not** performed. Also there is no check for a denominator of `0`. If these
     /// features are needed, use [`Fraction::new_reduce()`]
     pub const fn new(numerator: u32, denominator: u32) -> Self {
-        Self(Ratio::new_raw(numerator, denominator))
+        Self {
+            numer: numerator,
+            denom: denominator,
+        }
+    }
+
+    fn _new(numer: u32, denom: u32) -> Self {
+        let mut ret = Self::new(numer, denom);
+        ret.reduce();
+        ret
+    }
+
+    fn reduce(&mut self) {
+        if self.denom == 0 {
+            panic!("denominator == 0");
+        }
+        if self.numer == 0 {
+            self.denom = 1;
+            return;
+        }
+        if self.numer == self.denom {
+            self.numer = 1;
+            self.denom = 1;
+            return;
+        }
+        let g = gcd(self.numer, self.denom);
+
+        self.numer = self.numer / g;
+        self.denom = self.denom / g;
     }
 
     /// Return the numerator of the fraction
     pub const fn numerator(&self) -> &u32 {
-        self.0.numer()
+        &self.numer
     }
 
     /// Return the denominator of the fraction
     pub const fn denominator(&self) -> &u32 {
-        self.0.denom()
+        &self.denom
     }
 
     const fn const_eq(&self, other: &Self) -> bool {
-        (*self.numerator() as u64) * (*other.denominator() as u64)
-            == (*self.denominator() as u64) * (*other.numerator() as u64)
+        (self.numer as u64) * (other.denom as u64) == (self.denom as u64) * (other.numer as u64)
     }
 
     const fn const_cmp(&self, other: &Self) -> Ordering {
-        let ad = (*self.numerator() as u64) * (*other.denominator() as u64);
-        let bc = (*self.denominator() as u64) * (*other.numerator() as u64);
+        let ad = (self.numer as u64) * (other.denom as u64);
+        let bc = (self.denom as u64) * (other.numer as u64);
         if ad < bc {
             Ordering::Less
         } else if ad == bc {
             Ordering::Equal
         } else {
             Ordering::Greater
+        }
+    }
+}
+
+use core::hash::{Hash, Hasher};
+impl Hash for Fraction {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        recurse(self.numer, self.denom, state);
+
+        fn recurse<H: Hasher>(numer: u32, denom: u32, state: &mut H) {
+            if denom != 0 {
+                let (int, rem) = ((numer / denom), (numer % denom));
+                int.hash(state);
+                recurse(denom, rem, state);
+            } else {
+                denom.hash(state);
+            }
         }
     }
 }
@@ -86,28 +134,32 @@ impl Fraction {
     /// [`ConversionError::DivByZero`] : A `0` denominator was detected
     // TODO: add example
     pub fn new_reduce(numerator: u32, denominator: u32) -> Result<Self, ConversionError> {
-        if !denominator.is_zero() {
-            Ok(Self(Ratio::new(numerator, denominator)))
+        if denominator != 0 {
+            Ok(Self::_new(numerator, denominator))
         } else {
             Err(ConversionError::DivByZero)
         }
     }
 
     /// Returns the value truncated to an integer
-    pub fn to_integer(&self) -> u32 {
-        self.0.to_integer()
+    pub const fn to_integer(&self) -> u32 {
+        self.numer / self.denom
     }
 
     /// Constructs a `Fraction` from an integer.
     ///
     /// Equivalent to `Fraction::new(value,1)`.
-    pub fn from_integer(value: u32) -> Self {
-        Self(Ratio::from_integer(value))
+    pub const fn from_integer(value: u32) -> Self {
+        Self::new(value, 1)
     }
 
     /// Returns the reciprocal of the fraction
     pub fn recip(self) -> Self {
-        Self(self.0.recip())
+        if self.numer == 0 {
+            panic!("division by zero")
+        } else {
+            Self::new(self.denom, self.numer)
+        }
     }
 
     /// Checked `Fraction` × `Fraction` = `Fraction`
@@ -125,8 +177,25 @@ impl Fraction {
     /// assert_eq!(Fraction::new(u32::MAX, 1).checked_mul(&Fraction::new(2,1)),
     ///     None);
     /// ```
-    pub fn checked_mul(&self, v: &Self) -> Option<Self> {
-        self.0.checked_mul(&v.0).map(Self)
+    pub fn checked_mul(&self, rhs: &Self) -> Option<Self> {
+        let gcd_ad = gcd(self.numer, rhs.denom);
+        let gcd_bc = gcd(self.denom, rhs.numer);
+        Some(Self::_new(
+            (self.numer / gcd_ad).checked_mul(rhs.numer / gcd_bc)?,
+            (self.denom / gcd_bc).checked_mul(rhs.denom / gcd_ad)?,
+        ))
+    }
+
+    const fn is_zero(&self) -> bool {
+        self.numer == 0
+    }
+
+    const fn zero() -> Self {
+        Self::new(0, 1)
+    }
+
+    const fn one() -> Self {
+        Self::new(1, 1)
     }
 
     /// Checked `Fraction` / `Fraction` = `Fraction`
@@ -144,8 +213,35 @@ impl Fraction {
     /// assert_eq!(Fraction::new(1, u32::MAX).checked_div(&Fraction::new(2,1)),
     ///     None);
     /// ```
-    pub fn checked_div(&self, v: &Self) -> Option<Self> {
-        self.0.checked_div(&v.0).map(Self)
+    pub fn checked_div(&self, rhs: &Self) -> Option<Self> {
+        if rhs.is_zero() {
+            return None;
+        }
+        let (numer, denom) = if self.denom == rhs.denom {
+            (self.numer, rhs.numer)
+        } else if self.numer == rhs.numer {
+            (rhs.denom, self.denom)
+        } else {
+            let gcd_ac = gcd(self.numer, rhs.numer);
+            let gcd_bd = gcd(self.denom, rhs.denom);
+            (
+                (self.numer / gcd_ac).checked_mul(rhs.denom / gcd_bd)?,
+                (self.denom / gcd_bd).checked_mul(rhs.numer / gcd_ac)?,
+            )
+        };
+        // Manual `reduce()`, avoiding sharp edges
+        if denom == 0 {
+            None
+        } else if numer == 0 {
+            Some(Self::zero())
+        } else if numer == denom {
+            Some(Self::one())
+        } else {
+            let g = gcd(numer, denom);
+            let numer = numer / g;
+            let denom = denom / g;
+            Some(Self::new(numer, denom))
+        }
     }
 }
 
@@ -154,7 +250,7 @@ impl ops::Mul<Fraction> for u32 {
 
     /// Panicky u32 × `Fraction` = u32
     fn mul(self, rhs: Fraction) -> Self::Output {
-        (rhs.0 * self).to_integer()
+        (rhs * self).to_integer()
     }
 }
 
@@ -164,7 +260,7 @@ impl ops::Div<Fraction> for u32 {
     /// Panicky u32 / `Fraction` = u32
     #[allow(clippy::suspicious_arithmetic_impl)]
     fn div(self, rhs: Fraction) -> Self::Output {
-        (rhs.0.recip() * self).to_integer()
+        (rhs.recip() * self).to_integer()
     }
 }
 
@@ -173,7 +269,10 @@ impl ops::Mul<Fraction> for u64 {
 
     /// Panicky u64 × `Fraction` = u64
     fn mul(self, rhs: Fraction) -> Self::Output {
-        (Ratio::new_raw((*rhs.numerator()).into(), (*rhs.denominator()).into()) * self).to_integer()
+        let numer = rhs.numer as u64;
+        let denom = rhs.denom as u64;
+        let gcd = gcd_u64(denom, self);
+        (numer * (self / gcd)) / (denom / gcd)
     }
 }
 
@@ -183,7 +282,10 @@ impl ops::Div<Fraction> for u64 {
     /// Panicky u64 / `Fraction` = u64
     #[allow(clippy::suspicious_arithmetic_impl)]
     fn div(self, rhs: Fraction) -> Self::Output {
-        (Ratio::new_raw((*rhs.denominator()).into(), (*rhs.numerator()).into()) * self).to_integer()
+        let numer = rhs.denom as u64;
+        let denom = rhs.numer as u64;
+        let gcd = gcd_u64(denom, self);
+        (numer * (self / gcd)) / (denom / gcd)
     }
 }
 
@@ -197,7 +299,21 @@ impl ops::Mul for Fraction {
     /// The same reason the integer operation would panic. Namely, if the
     /// result overflows the type.
     fn mul(self, rhs: Self) -> Self::Output {
-        Self(self.0 * rhs.0)
+        let gcd_ad = gcd(self.numer, rhs.denom);
+        let gcd_bc = gcd(self.denom, rhs.numer);
+        Self::_new(
+            self.numer / gcd_ad * (rhs.numer / gcd_bc),
+            self.denom / gcd_bc * (rhs.denom / gcd_ad),
+        )
+    }
+}
+
+impl ops::Mul<u32> for Fraction {
+    type Output = Self;
+    #[inline]
+    fn mul(self, rhs: u32) -> Self {
+        let gcd = gcd(self.denom, rhs);
+        Self::_new(self.numer * (rhs / gcd), self.denom / gcd)
     }
 }
 
@@ -211,7 +327,12 @@ impl ops::Div for Fraction {
     /// The same reason the integer operation would panic. Namely, if the
     /// result overflows the type.
     fn div(self, rhs: Self) -> Self::Output {
-        Self(self.0 / rhs.0)
+        let gcd_ac = gcd(self.numer, rhs.numer);
+        let gcd_bd = gcd(self.denom, rhs.denom);
+        Self::_new(
+            self.numer / gcd_ac * (rhs.denom / gcd_bd),
+            self.denom / gcd_bd * (rhs.numer / gcd_ac),
+        )
     }
 }
 
@@ -219,6 +340,56 @@ impl Default for Fraction {
     fn default() -> Self {
         Self::new(1, 1)
     }
+}
+
+const fn gcd(mut m: u32, mut n: u32) -> u32 {
+    // Use Stein's algorithm
+    if m == 0 || n == 0 {
+        return m | n;
+    }
+
+    // find common factors of 2
+    let shift = (m | n).trailing_zeros();
+
+    // divide n and m by 2 until odd
+    m >>= m.trailing_zeros();
+    n >>= n.trailing_zeros();
+
+    while m != n {
+        if m > n {
+            m -= n;
+            m >>= m.trailing_zeros();
+        } else {
+            n -= m;
+            n >>= n.trailing_zeros();
+        }
+    }
+    m << shift
+}
+
+const fn gcd_u64(mut m: u64, mut n: u64) -> u64 {
+    // Use Stein's algorithm
+    if m == 0 || n == 0 {
+        return m | n;
+    }
+
+    // find common factors of 2
+    let shift = (m | n).trailing_zeros();
+
+    // divide n and m by 2 until odd
+    m >>= m.trailing_zeros();
+    n >>= n.trailing_zeros();
+
+    while m != n {
+        if m > n {
+            m -= n;
+            m >>= m.trailing_zeros();
+        } else {
+            n -= m;
+            n >>= n.trailing_zeros();
+        }
+    }
+    m << shift
 }
 
 #[cfg(test)]
